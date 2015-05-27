a3f71e41c990ef185e69a27b7e155625146859fc
LE BAIL Baptiste <blebail@iocean.fr>
2015-05-27 21:54:35 +0200

    Gradle set up

---
+++ commons/src/main/java/com/daeliin/framework/commons/Main.java
@@ -0,0 +1,10 @@
+package com.daeliin.framework.commons;
+
+/**
+ *
+ */
+public class Main {
+    public static void main(String args[]) {
+        System.out.println("I'm the commons project.");
+    }
+}
---
+++ webapp/src/main/java/com/daeliin/framework/Main.java
@@ -0,0 +1,11 @@
+package com.daeliin.framework;
+
+/**
+ *
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("\nExecuting webapp project.\n");
+    }
+}
