de22dc01f1212e1775dc8692bb790bf10a0da2a2
Baptiste Le Bail <baptiste.lebail@linuxmail.org>
2015-08-06 00:12:09 +0200

    OAuth2 initialization

--- sample/src/main/java/com/daeliin/framework/sample/Application.java
+++ sample/src/main/java/com/daeliin/framework/sample/Application.java
@@ -1,10 +1,11 @@
 package com.daeliin.framework.sample;
 
+import com.daeliin.framework.security.oauth2.AuthenticationServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class Application extends AuthenticationServer {
     
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
---
+++ security/src/main/java/com/daeliin/framework/security/oauth2/AuthenticationServer.java
@@ -0,0 +1,55 @@
+package com.daeliin.framework.security.oauth2;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableResourceServer
+public class AuthenticationServer {
+    
+    @Configuration
+    @EnableAuthorizationServer
+    protected static class OAuth2Config extends AuthorizationServerConfigurerAdapter {
+
+        @Autowired
+        private AuthenticationManager authenticationManager;
+
+        @Override
+        public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+                endpoints.authenticationManager(authenticationManager);
+        }
+
+        @Override
+        public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+            clients.inMemory()
+                .withClient("my-trusted-client")
+                    .authorizedGrantTypes("password", "authorization_code", "refresh_token", "implicit")
+                    .authorities("ROLE_CLIENT", "ROLE_TRUSTED_CLIENT")
+                    .scopes("read", "write", "trust")
+                    .resourceIds("oauth2-resource")
+                    .accessTokenValiditySeconds(600)
+            .and()
+                .withClient("my-client-with-registered-redirect")
+                    .authorizedGrantTypes("authorization_code")
+                    .authorities("ROLE_CLIENT")
+                    .scopes("read", "trust")
+                    .resourceIds("oauth2-resource")
+                    .redirectUris("http://anywhere?key=value")
+            .and()
+                .withClient("my-client-with-secret")
+                    .authorizedGrantTypes("client_credentials", "password")
+                    .authorities("ROLE_CLIENT")
+                    .scopes("read")
+                    .resourceIds("oauth2-resource")
+                    .secret("secret");
+        }
+    }
+}
