3ee67fee76338e41bf20545ccef9fd95f9b6e219
LE BAIL Baptiste <blebail@iocean.fr>
2016-02-07 16:26:35 +0100

    Typo

--- core/src/test/java/com/daeliin/framework/core/Application.java
+++ core/src/test/java/com/daeliin/framework/core/Application.java
@@ -2,7 +2,7 @@ package com.daeliin.framework.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//
+
 @SpringBootApplication
 public class Application {
     
