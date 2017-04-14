fd6a7471bf62cfc0cb66cdd55417a556455c8dba
baptistelebail <baptiste.lebail@linuxmail.org>
2017-04-14 17:05:26 +0200

    (core) ResourceService is now 0.1 ISO

--- components-core/src/test/java/com/daeliin/components/core/resource/service/ResourceServiceTest.java
+++ components-core/src/test/java/com/daeliin/components/core/resource/service/ResourceServiceTest.java
@@ -6,7 +6,9 @@ import com.daeliin.components.core.fake.UUIDEntity;
 import com.daeliin.components.core.fake.UUIDEntityRepository;
 import com.daeliin.components.core.fake.UUIDEntityService;
 import com.daeliin.components.core.fixtures.UUIDEntityFixtures;
-import org.apache.catalina.User;
+import com.daeliin.components.domain.pagination.PageRequest;
+import com.daeliin.components.domain.pagination.Sort;
+import com.google.common.collect.Sets;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
@@ -66,31 +68,6 @@ public class ResourceServiceTest extends AbstractTransactionalJUnit4SpringContex
 
         assertThat(createdUuidEntities).containsOnly(newUuidEntity1, newUuidEntity2);
     }
-//
-//    @Test
-//    public void create_invalidResources_callsRepositorySaveAndThrowsConstraintViolationException() {
-//        List<User> invalidUsers =
-//            Arrays.asList(
-//                new User().withName(""),
-//                new User().withName(""));
-//
-//        Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
-//        Set<? extends ConstraintViolation<User>> constraintViolations = new HashSet<>();
-//        invalidUsers.forEach((User invalidUser) -> constraintViolations.addAll((Set)validator.validateProperty(invalidUser, "name")));
-//
-//        when(repository.save(invalidUsers))
-//            .thenThrow(
-//                new ConstraintViolationException(constraintViolations));
-//
-//        try {
-//            service.create(invalidUsers);
-//        } catch (ConstraintViolationException e) {
-//            verify(repository).save(invalidUsers);
-//            return;
-//        }
-//
-//        fail();
-//    }
 
     @Test
     public void shouldCheckThatNullDoesntExist() {
@@ -124,12 +101,12 @@ public class ResourceServiceTest extends AbstractTransactionalJUnit4SpringContex
     }
 
     @Test(expected = PersistentResourceNotFoundException.class)
-    public void shouldThrowResourceNotFoundException_whenIdIsNull() {
+    public void shouldThrowPersistentResourceNotFoundException_whenIdIsNull() {
         service.findOne(null);
     }
 
     @Test
-    public void shouldThrowResourceNotFoundException_whenResourceIdDoesntExist() {
+    public void shouldThrowPersistentResourceNotFoundException_whenResourceIdDoesntExist() {
         doThrow(new PersistentResourceNotFoundException("")).when(repository).findOne(-1L);
 
         try {
@@ -151,175 +128,122 @@ public class ResourceServiceTest extends AbstractTransactionalJUnit4SpringContex
         assertThat(service.findOne(existingUuidEntity.id())).isEqualTo(existingUuidEntity);
         verify(repository).findOne(existingUuidEntity.id());
     }
-//
-//    @Test
-//    public void findAll_callsRepositoryFindAllAndReturnsAllResources() {
-//        List<User> allUsers =
-//            Arrays.asList(
-//                new User().withId(1L).withName("userName1"),
-//                new User().withId(2L).withName("userName2"),
-//                new User().withId(3L).withName("userName3"));
-//
-//        when(repository.findAll()).thenReturn(allUsers);
-//
-//        List<User> users = (List<User>)service.findAll();
-//
-//        verify(repository).findAll();
-//        assertEquals(users, allUsers);
-//    }
-//
-//    @Test
-//    public void findAll_pageRequest_callsRepositoryFindAllWithPageRequest() {
-//        PageRequest userPageRequest = new PageRequest(1, 10, Sort.Direction.ASC, "id", "name");
-//
-//        service.findAll(userPageRequest);
-//        verify(repository).findAll(userPageRequest);
-//    }
-//
-//    @Test
-//    public void findAll_multipleResourcesIds_callsRepositoryFindAllWithMultipleResourcesIds() {
-//        List<Long> usersIds = Arrays.asList(1L, 2L);
-//
-//        service.findAll(usersIds);
-//        verify(repository).findAll(usersIds);
-//    }
-//
-//    @Test(expected = ResourceNotFoundException.class)
-//    public void update_nullResourceId_throwsResourceNotFoundException() {
-//        service.update(null, null);
-//    }
-//
-//    @Test
-//    public void update_nonExistingResourceId_callsRepositoryExistsAndThrowsResourceNotFoundException() {
-//        when(repository.exists(-1L)).thenReturn(false);
-//
-//        try {
-//            service.update(-1L, null);
-//        } catch (ResourceNotFoundException e) {
-//            verify(repository).exists(-1L);
-//            return;
-//        }
-//
-//        fail();
-//    }
-//
-//    @Test(expected = ResourceNotFoundException.class)
-//    public void update_nullResource_throwsResourceNotFoundException() {
-//        when(repository.exists(1L)).thenReturn(true);
-//
-//        service.update(1L, null);
-//    }
-//
-//    @Test(expected = ResourceNotFoundException.class)
-//    public void update_existingResourceIdButDiffersFromActualResourceId_throwsResourceNotFoundException() {
-//        when(repository.exists(1L)).thenReturn(true);
-//
-//        service.update(1L, new User().withId(2L).withName("userName"));
-//    }
-//
-//    @Test
-//    public void update_existingResourceIdAndValidResource_callsRepositorySaveWithResourceAndReturnsResource() {
-//        User user = new User().withId(1L).withName("userName");
-//        when(repository.exists(user.getId())).thenReturn(true);
-//        when(repository.save(user)).thenReturn(user);
-//
-//        User updatedUser = service.update(user.getId(), user);
-//
-//        verify(repository).save(user);
-//        assertEquals(updatedUser, user);
-//    }
-//
-//    @Test
-//    public void update_multipleResources_callsRepositorySaveWithMultipleResources() {
-//        List<User> users =
-//            Arrays.asList(
-//                new User().withId(1L).withName("userName1"),
-//                new User().withId(2L).withName("userName2"));
-//
-//        when(repository.save(users)).thenReturn(users);
-//
-//        Iterable<User> updatedUsers = service.update(users);
-//
-//        verify(repository).save(users);
-//        assertEquals(updatedUsers, users);
-//    }
-//
-//    @Test(expected = ResourceNotFoundException.class)
-//    public void delete_nonExistingResourceId_throwsResourceNotFoundException() {
-//        when(repository.exists(-1L)).thenReturn(false);
-//
-//        service.delete(-1L);
-//    }
-//
-//    @Test
-//    public void delete_existingResourceId_callsRepositoryExistsAndDelete() {
-//        when(repository.exists(1L)).thenReturn(true);
-//
-//        service.delete(1L);
-//        verify(repository).exists(1L);
-//        verify(repository).delete(1L);
-//    }
-//
-//    @Test
-//    public void delete_existingResourceIds_deletesResources() {
-//        User existingUser1 = new User().withId(1L).withName("userName1");
-//        User existingUser2 = new User().withId(2L).withName("userName2");
-//
-//        when(repository.findAll(Arrays.asList(1L, 2L))).thenReturn(Arrays.asList(existingUser1, existingUser2));
-//        service.delete(Arrays.asList(1L, 2L));
-//
-//        verify(repository).delete(existingUser1);
-//        verify(repository).delete(existingUser2);
-//    }
-//
-//    @Test
-//    public void delete_nonExistingResourceIds_doesntCallRepositoryDeleteWithResourceIds() {
-//        User nonExistingUser1 = new User().withId(-1L).withName("userName1");
-//        User nonExistingUser2 = new User().withId(-2L).withName("userName2");
-//
-//        when(repository.findAll(Arrays.asList(-1L, -2L))).thenReturn(new ArrayList<>());
-//        service.delete(Arrays.asList(-1L, -2L));
-//
-//        verify(repository, never()).delete(nonExistingUser1);
-//        verify(repository, never()).delete(nonExistingUser2);
-//    }
-//
-//    @Test
-//    public void delete_nonExistingAndExistingResourceIds_deletesOnlyExistingResources() {
-//        User existingUser1 = new User().withId(1L).withName("userName1");
-//        User existingUser2 = new User().withId(2L).withName("userName2");
-//        User nonExistingUser = new User().withId(-3L).withName("userName3");
-//
-//        when(repository.findAll(Arrays.asList(1L, 2L, -3L))).thenReturn(Arrays.asList(existingUser1, existingUser2));
-//        service.delete(Arrays.asList(1L, 2L, -3L));
-//
-//        verify(repository).delete(existingUser1);
-//        verify(repository).delete(existingUser2);
-//        verify(repository, never()).delete(nonExistingUser);
-//    }
-//
-//    @Test
-//    public void delete_resource_callsRepositoryDeleteWithResource() {
-//        User user = new User().withId(1L).withName("userName");
-//
-//        service.delete(user);
-//        verify(repository).delete(user);
-//    }
-//
-//    @Test
-//    public void delete_multipleResources_callsRepositoryDeleteWithResources() {
-//        List<User> users =
-//            Arrays.asList(
-//                new User().withId(1L).withName("userName1"),
-//                new User().withId(2L).withName("userName2"));
-//
-//        service.delete(users);
-//        verify(repository).delete(users);
-//    }
-//
-//    @Test
-//    public void deleteAll_callsRepositoryDeleteAll() {
-//        service.deleteAll();
-//        verify(repository).deleteAll();
-//    }
+
+    @Test
+    public void shouldCallRepositoryFindAllAndReturnsAllResources_whenFindingAllResources() {
+        Collection<UUIDEntity> allUuidEntities = Arrays.asList(
+                UUIDEntityFixtures.uuidEntity1(),
+                UUIDEntityFixtures.uuidEntity2(),
+                UUIDEntityFixtures.uuidEntity3(),
+                UUIDEntityFixtures.uuidEntity4());
+
+        when(repository.findAll()).thenReturn(allUuidEntities);
+
+        Collection<UUIDEntity> foundUuidEntities = service.findAll();
+
+        verify(repository).findAll();
+        assertThat(foundUuidEntities).containsOnly(
+                UUIDEntityFixtures.uuidEntity1(),
+                UUIDEntityFixtures.uuidEntity2(),
+                UUIDEntityFixtures.uuidEntity3(),
+                UUIDEntityFixtures.uuidEntity4());
+    }
+
+    @Test
+    public void shouldCallRepositoryFindAllWithPageRequest_whenFindingAllResoucesWithPageRequest() {
+        PageRequest pageRequest = new PageRequest(1, 10, Sets.newHashSet(new Sort("id", Sort.Direction.ASC)));
+
+        service.findAll(pageRequest);
+        verify(repository).findAll(pageRequest);
+    }
+
+    @Test
+    public void shouldCallRepositoryFindAllWithResourceIds_whenFindingResources() {
+        Collection<Long> uuidEntityIds = Arrays.asList(UUIDEntityFixtures.uuidEntity1().id(), UUIDEntityFixtures.uuidEntity2().id());
+
+        service.findAll(uuidEntityIds);
+        verify(repository).findAll(uuidEntityIds);
+    }
+
+    @Test(expected = PersistentResourceNotFoundException.class)
+    public void shouldThrowPersistenceResourceNotFoundException_whenUpdatingNullResource() {
+        UUIDEntity nullUuidEntity = null;
+
+        service.update(nullUuidEntity);
+    }
+
+    @Test(expected = PersistentResourceNotFoundException.class)
+    public void shouldThrowPersistenceResourceNotFoundException_whenUpdatingNonExistingResource() {
+        UUIDEntity nonExistingUuidEntity = new UUIDEntity(-1L, UUID.randomUUID().toString(), LocalDateTime.now(), "label-1");
+
+        service.update(nonExistingUuidEntity);
+    }
+
+    @Test
+    public void shouldCallRepositorySaveWithResourceAndReturnResource_whenUpdatingResource() {
+        UUIDEntity exitingUuidEntity = UUIDEntityFixtures.uuidEntity1();
+        doReturn(true).when(repository).exists(exitingUuidEntity.id());
+        doReturn(exitingUuidEntity).when(repository).save(exitingUuidEntity);
+
+        UUIDEntity updatedUuidEntity = service.update(exitingUuidEntity);
+
+        verify(repository).save(exitingUuidEntity);
+        assertThat(updatedUuidEntity).isEqualTo(exitingUuidEntity);
+    }
+
+    @Test
+    public void shouldCallRepositorySaveWithMultipleResources_whenUpdatingResources() {
+        Collection<UUIDEntity> existingUuidEntities = Arrays.asList(UUIDEntityFixtures.uuidEntity1(), UUIDEntityFixtures.uuidEntity2());
+
+        when(repository.save(existingUuidEntities)).thenReturn(existingUuidEntities);
+
+        Collection<UUIDEntity> updatedUuidEntities = service.update(existingUuidEntities);
+
+        verify(repository).save(existingUuidEntities);
+        assertThat(updatedUuidEntities).isEqualTo(existingUuidEntities);
+    }
+
+    @Test
+    public void shouldCallRepositoryDelete_whenDeletingResource() {
+        service.delete(1L);
+        verify(repository).delete(1L);
+    }
+
+    @Test
+    public void shouldReturnFalse_whenDeletingNoResources() {
+        assertThat(service.delete(Arrays.asList())).isFalse();
+
+    }
+
+    @Test
+    public void shouldCallRepositoryDeleteWithResourceIds_whenDeletingResources() {
+        UUIDEntity uuidEntity1 = UUIDEntityFixtures.uuidEntity1();
+        UUIDEntity uuidEntity2 = UUIDEntityFixtures.uuidEntity2();
+
+        service.delete(Arrays.asList(uuidEntity1.id(), uuidEntity2.id()));
+
+        verify(repository).delete(Arrays.asList(uuidEntity1.id(), uuidEntity2.id()));
+    }
+
+    @Test
+    public void shouldCallRepositoryDeleteWithResource_whenDeletingResource() {
+        UUIDEntity uuidEntity = UUIDEntityFixtures.uuidEntity1();
+
+        service.delete(uuidEntity);
+        verify(repository).delete(uuidEntity.id());
+    }
+
+    @Test
+    public void dshouldCallRepositoryDeleteWithResources_whenDeletingResources() {
+        Collection<Long> uuidEntityIds = Arrays.asList(UUIDEntityFixtures.uuidEntity1().id(), UUIDEntityFixtures.uuidEntity2().id());
+
+        service.delete(uuidEntityIds);
+        verify(repository).delete(uuidEntityIds);
+    }
+
+    @Test
+    public void shouldCallRepositoryDeleteAll_whenDeletingAllResources() {
+        service.deleteAll();
+        verify(repository).deleteAll();
+    }
 }
