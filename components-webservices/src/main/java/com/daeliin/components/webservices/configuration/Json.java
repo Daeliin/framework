812e17755114f6b7081cefdbb696a1da06416771
baptistelebail <baptiste.lebail@linuxmail.org>
2017-05-01 15:26:26 +0200

    (webservices) JsonMapper configuration

---
+++ components-webservices/src/main/java/com/daeliin/components/webservices/configuration/Json.java
@@ -0,0 +1,30 @@
+package com.daeliin.components.webservices.configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.guava.GuavaModule;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+@Configuration
+public class Json {
+
+    @Bean
+    @Primary
+    public ObjectMapper jsonMapper(Jackson2ObjectMapperBuilder builder) {
+        ObjectMapper objectMapper = builder.createXmlMapper(false).build();
+
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        objectMapper.registerModule(new ParameterNamesModule());
+        objectMapper.registerModule(new Jdk8Module());
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.registerModule(new GuavaModule());
+
+        return objectMapper;
+    }
+}
