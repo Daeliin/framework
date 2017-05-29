c20c40d3fd5b9341921ecd17f88d24a25273d9d5
baptistelebail <baptiste.lebail@linuxmail.org>
2017-05-29 13:02:30 +0200

    (core) countries are now cached

--- components-core/src/main/java/com/daeliin/components/core/country/CountryService.java
+++ components-core/src/main/java/com/daeliin/components/core/country/CountryService.java
@@ -1,16 +1,45 @@
 package com.daeliin.components.core.country;
 
-import com.daeliin.components.core.resource.service.ResourceService;
-import com.daeliin.components.core.sql.BCountry;
+import com.daeliin.components.core.exception.PersistentResourceNotFoundException;
+import com.google.common.base.Strings;
 import org.springframework.stereotype.Component;
 
 import javax.inject.Inject;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Component
-public final class CountryService extends ResourceService<Country, BCountry, String, CountryRepository> {
+public final class CountryService {
+
+    private final Map<String, Country> countryByCode;
+    private final CountryRepository repository;
+    private final CountryConversion conversion;
 
     @Inject
     public CountryService(CountryRepository repository) {
-        super(repository, new CountryConversion());
+        this.repository = repository;
+        this.conversion = new CountryConversion();
+        this.countryByCode = new ConcurrentHashMap<>();
+    }
+
+    public Country findByCode(String countryCode) {
+        if (countryByCode.isEmpty()) {
+            invalidateCache();
+        }
+
+        if (Strings.isNullOrEmpty(countryCode) || !countryByCode.containsKey(countryCode)) {
+            throw new PersistentResourceNotFoundException(String.format("There is no country for country code %s", countryCode));
+        }
+
+        return countryByCode.get(countryCode);
+    }
+
+    private void invalidateCache() {
+        countryByCode.clear();
+
+        repository.findAll()
+                .stream()
+                .map(conversion::instantiate)
+                .forEach(country -> countryByCode.put(country.code, country));
     }
 }
--- components-core/src/test/java/com/daeliin/components/core/country/CountryServiceTest.java
+++ components-core/src/test/java/com/daeliin/components/core/country/CountryServiceTest.java
@@ -1,14 +1,40 @@
 package com.daeliin.components.core.country;
 
-import com.daeliin.components.core.resource.service.ResourceService;
+import com.daeliin.components.core.Application;
+import com.daeliin.components.core.exception.PersistentResourceNotFoundException;
+import com.daeliin.components.core.library.CountryLibrary;
 import org.junit.Test;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+
+import javax.inject.Inject;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public final class CountryServiceTest {
+@ContextConfiguration(classes = Application.class)
+public class CountryServiceTest extends AbstractTransactionalJUnit4SpringContextTests {
+
+    @Inject
+    private CountryService countryService;
+
+    @Test(expected = PersistentResourceNotFoundException.class)
+    public void shouldThrowException_whenFindingNonExistingCountryCode() {
+        countryService.findByCode("nope");
+    }
+
+    @Test(expected = PersistentResourceNotFoundException.class)
+    public void shouldThrowException_whenFindingNullCountryCode() {
+        String nullContryCode = null;
+
+        countryService.findByCode(nullContryCode);
+    }
 
     @Test
-    public void shouldExtendResourceService() {
-        assertThat(CountryService.class.getSuperclass().getClass()).isEqualTo(ResourceService.class.getClass());
+    public void shouldFindACountryByCode() {
+        Country france = CountryLibrary.france();
+
+        Country foundCountry = countryService.findByCode(france.code);
+
+        assertThat(foundCountry).isEqualTo(france);
     }
 }
