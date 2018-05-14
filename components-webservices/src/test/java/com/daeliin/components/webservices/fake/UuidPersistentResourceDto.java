2ecdd9d86f1a38796a9d108d66ca43bd2d9b5d33
baptistelebail <baptiste.lebail@linuxmail.org>
2018-05-14 16:52:25 +0200

    (chore) NotBlank from javax.api and not hibernate

--- components-webservices/src/test/java/com/daeliin/components/webservices/fake/UuidPersistentResourceDto.java
+++ components-webservices/src/test/java/com/daeliin/components/webservices/fake/UuidPersistentResourceDto.java
@@ -1,8 +1,7 @@
 package com.daeliin.components.webservices.fake;
 
 
-import org.hibernate.validator.constraints.NotBlank;
-
+import javax.validation.constraints.NotBlank;
 import java.time.Instant;
 
 public final class UuidPersistentResourceDto {
