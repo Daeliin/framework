2332f5329b95b2df84d5e20a5d2f8ed0d26eb572
baptistelebail <blebail@protonmail.com>
2020-03-13 23:38:42 +0100

    (test) Do nothing in after() of SqlMemoryDatabase

--- components-test/src/main/java/com/blebail/components/test/rule/SqlMemoryDatabase.java
+++ components-test/src/main/java/com/blebail/components/test/rule/SqlMemoryDatabase.java
@@ -4,7 +4,6 @@ import com.ninja_squad.dbsetup.DbSetup;
 import com.ninja_squad.dbsetup.Operations;
 import com.ninja_squad.dbsetup.destination.DataSourceDestination;
 import org.h2.jdbcx.JdbcDataSource;
-import org.junit.jupiter.api.extension.AfterAllCallback;
 import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 
@@ -15,10 +14,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.Properties;
 
-public final class SqlMemoryDatabase implements BeforeAllCallback, AfterAllCallback {
+public final class SqlMemoryDatabase implements BeforeAllCallback {
 
     private static final String APPLICATION_PROPERTIES_PATH = "/application.properties";
 
@@ -32,11 +30,6 @@ public final class SqlMemoryDatabase implements BeforeAllCallback, AfterAllCallb
     public void beforeAll(ExtensionContext extensionContext) throws Exception {
         loadDataSource();
         createSchema();
-    }
-
-    @Override
-    public void afterAll(ExtensionContext extensionContext) {
-        closeConnection();
     }
 
     private void loadDataSource() throws Exception {
@@ -56,14 +49,6 @@ public final class SqlMemoryDatabase implements BeforeAllCallback, AfterAllCallb
         String createSchemaSql = Files.readString(schemaPath);
 
         new DbSetup(new DataSourceDestination(dataSource), Operations.sql(createSchemaSql)).launch();
-    }
-
-    private void closeConnection() {
-        try {
-            dataSource.getConnection().close();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
     }
 
     public DataSource dataSource() {
